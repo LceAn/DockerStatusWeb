21a7f94c44b6fea0fc907fb74f31c461d7482c11
LceAn <63484787+LceAn@users.noreply.github.com>
2024-08-07 18:33:23 +0800

    Add files via upload
    v0.0.2

---
+++ v0.0.2/app/config.py
@@ -0,0 +1,8 @@
+# -*-  coding : utf-8 -*-
+# @Time : 2024/8/7 下午6:06
+# @Autor : LceAn
+# @File : config.py
+# @Software : PyCharm
+
+
+DOCKER_API_URL = 'http://localhost:2375'
---
+++ v0.0.2/app/views/__init__.py
@@ -0,0 +1,8 @@
+# -*-  coding : utf-8 -*-
+# @Time : 2024/8/7 下午6:01
+# @Autor : LceAn
+# @File : __init__.py.py
+# @Software : PyCharm
+
+
+from .main import *
